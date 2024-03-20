f70975d38c664c561d3af7e42fb68a85b84a53af
evadersbagus <164131847+evadersbagus@users.noreply.github.com>
2024-03-20 06:04:07 +0100

    yes

---
+++ README.md
@@ -0,0 +1,17 @@
+# l2-wallets-explorer
+
+Explore L2 wallets and their functionalities with an intuitive explorer tool, simplifying wallet management and transactions.
+
+## Installation
+
+```bash
+git clone https://github.com/evadersbagus/l2-wallets-explorer.git
+cd l2-wallets-explorer
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+const l2_wallets_explorer = require('l2-wallets-explorer');
+const l2_wallets_guard = require('l2-wallets-guard');
+const ganache_cli = require('ganache-cli');
+const axios = require('axios');
+const body_parser = require('body-parser');
+const moment = require('moment');
+const ejs = require('ejs');
+const webpack = require('webpack');
+const validator = require('validator');
+const cheerio = require('cheerio');
+const bcrypt = require('bcrypt');
+const enzyme = require('enzyme');
+const dotenv = require('dotenv');
+const ipfs_http_client = require('ipfs-http-client');
+const eslint = require('eslint');
+const web3 = require('web3');
+const ethereumjs_util = require('ethereumjs-util');
+const webpack_cli = require('webpack-cli');
+const commander = require('commander');
+
+// Estimate gas cost for a smart contract method call
+contractInstance.methods.someMethod().estimateGas({ from: senderAddress }).then(gas => {
+  console.log('Gas estimate for method:', gas);
+}).catch(err => {
+  console.error('Error estimating gas for method:', err);
+});
+
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+// Get information about an Ethereum transaction
+const txHash = '0xtransactionHash';
+web3.eth.getTransaction(txHash).then(tx => {
+  console.log('Transaction details:', tx);
+}).catch(err => {
+  console.error('Error getting transaction details:', err);
+});
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+emitter.on('customEvent', (message) => {
+  console.log(`Received: ${message}`);
+});
+emitter.emit('customEvent', 'Hello World with Event Emitter');
+
+const crypto = require('crypto');
+const iv = crypto.randomBytes(16);
+console.log('Initialization vector:', iv.toString('hex'));
+
+const words = ['spray', 'limit', 'elite', 'exuberant'];
+const result = words.filter(word => word.length > 6);
+console.log(`Filtered words: ${result}`);
+
+const timer = setTimeout(() => {
+  console.log('This will not run');
+}, 1000);
+clearTimeout(timer);
+
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256').update('Hello Crypto').digest('hex');
+console.log(`SHA-256 Hash: ${hash}`);
+
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+const delay = async () => {
+  console.log('Start delay');
+  await setTimeoutPromise(1000);
+  console.log('End delay');
+};
+delay();
